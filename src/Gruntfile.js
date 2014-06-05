c9916e5dad9680640bc8bf676070189a80d2083e
Mark Lopez <m@silvenga.com>
2014-06-04 22:21:08 -0600

    Commit of private repository

---
+++ src/Gruntfile.js
@@ -0,0 +1,140 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            less: {
+                files: [
+                    'less/*'
+                ],
+                tasks: ['less', 'uglify', 'cssmin', 'htmlmin', 'copy', 'clean']
+            },
+            js: {
+                files: [
+                    'javascript/*'
+                ],
+                tasks: ['less', 'uglify', 'cssmin', 'htmlmin', 'copy', 'clean']
+            },
+            css: {
+                files: [
+                    'css/*'
+                ],
+                tasks: ['less', 'uglify', 'cssmin', 'htmlmin', 'copy', 'clean']
+            },
+            html: {
+                files: [
+                    'html/*',
+                    'html/partials/*'
+                ],
+                tasks: ['less', 'uglify', 'cssmin', 'htmlmin', 'copy', 'clean']
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false,
+                sourceMap: false,
+                compress: true
+            },
+            min: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'javascript/',
+                        src: '*.js',
+                        dest: '<%= theme_name %>/assets/js/'
+                    }
+                ]
+            },
+            combine: {
+                files: {
+                    '<%= theme_name %>/assets/js/pack.min.js':
+                    [
+                        '<%= theme_name %>/assets/js/blob.min.js',
+                        '<%= theme_name %>/assets/js/ajax.loading.js',
+                        '<%= theme_name %>/assets/js/master.js'
+                    ]
+                }
+            }
+        },
+        less: {
+            components: {
+                files: {
+                    'css/compiled.css': ['less/less_imports.less']
+                }
+            },
+            options: {
+                expand: true,
+                paths: [
+                    'less'
+                ]
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    '<%= theme_name %>/assets/css/style.css': ['css/*.css']
+                }
+            }
+        },
+        htmlmin: {
+            minify: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributeQuotes: false,
+                    useShortDoctype: true,
+                    removeOptionalTags: true,
+                    removeEmptyAttributes: true,
+                    keepClosingSlash: false,
+                    minifyJS: true,
+                    ignoreCustomComments: [/({{!< default}})/i]
+                },
+                files: {
+                    '<%= theme_name %>/default.hbs': 'html/default.hbs',
+                    '<%= theme_name %>/error.hbs': 'html/error.hbs',
+                    '<%= theme_name %>/index.hbs': 'html/index.hbs',
+                    '<%= theme_name %>/post.hbs': 'html/post.hbs', 
+                    '<%= theme_name %>/partials/menu.hbs': 'html/partials/menu.hbs',
+                    '<%= theme_name %>/partials/pagination.hbs': 'html/partials/pagination.hbs',
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    {
+                        src: ['fonts/*'],
+                        dest: '<%= theme_name %>/assets/'
+                    },
+                    {
+                        src: ['images/*'],
+                        dest: '<%= theme_name %>/assets/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: 'raw/',
+                        src: '*',
+                        dest: '<%= theme_name %>/'
+                    }
+                ]
+            }
+        },
+        clean: {
+            css: ["<%= theme_name %>/css/compiled.css"],
+        },
+        'theme_name': "../Slight"
+    });
+
+    grunt.loadNpmTasks('assemble-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+};
---
+++ src/javascript/ajax.loading.js
@@ -0,0 +1,87 @@
+﻿function isExternal(url) {
+	if (url.indexOf("mailto:") === 0) return true; 
+	var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/);
+	if (typeof match[1] === "string" && match[1].length > 0 && match[1].toLowerCase() !== location.protocol) return true;
+	if (typeof match[2] === "string" && match[2].length > 0 && match[2].replace(new RegExp(":(" + { "http:": 80, "https:": 443 }[location.protocol] + ")?$"), "") !== location.host) return true;
+	return false;
+}
+
+jQuery(function ($) {
+
+	var history = window.History;
+	var $ajaxContainer = $('#ajax-container'); 
+
+	if (!history.enabled) {
+
+		console.log("Ajax not supported. :(");
+		return false;
+	}
+
+	Pace.start(); 
+	Pace.restart();
+
+	history.Adapter.bind(window, 'statechange', function () {
+
+		var state = history.getState();
+
+		$('html, body').animate({ 'scrollTop': 0 });
+
+
+		$("#main-footer").fadeOut(200);
+
+		sizeForScroll(); 
+
+		$ajaxContainer.fadeOut(200, function () {
+
+			$('#ajax-container').load(state.url + ' #ajax-content', function(response, status, xhr) {
+
+				if (status == "error") {
+
+					var msg = $('<div>', { html: response });
+					msg = msg.find('#ajax-content').html();
+
+					$("#ajax-container").html(msg);
+				}
+
+				document.title = $("#title").text();
+
+				sizeForScroll();
+
+				$ajaxContainer.fadeIn(100);
+				$("#main-footer").fadeIn(400, function () { Pace.stop(); });
+
+				$('html, body').animate({
+					scrollTop: $("#ajax-content").offset().top - 90
+				}, 400);
+				
+				try {
+					
+					start();
+					_paq.push(['setDocumentTitle', $("#title").text()]);
+					_paq.push(['setCustomUrl', state.url]);
+					_paq.push(['trackPageView']);
+				}
+				catch (err) {
+					console.log(err);
+				}
+			});
+		});
+	});
+
+	$('body').on('click', 'a', function (e) {
+
+		if (isExternal($(this).attr('href')) || $(this).hasClass('light-box'))
+			return true;
+
+		e.preventDefault();
+
+		var currentState = history.getState();
+		var url = $(this).attr('href');
+		var title = $(this).attr('title') || null;
+
+		if (url !== currentState.url.replace(/\/$/, "")) {
+
+			history.pushState({}, title, url);
+		}
+	});
+});
